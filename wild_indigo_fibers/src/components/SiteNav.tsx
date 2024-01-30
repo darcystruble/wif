7b7fa51b0ca66838718bbbc3b2f6aa75821ab2cf
darcystruble <darcyhallstruble@gmail.com>
2024-01-30 13:31:09 -0500

    navbar set up

--- wild_indigo_fibers/src/components/SiteNav.tsx
+++ wild_indigo_fibers/src/components/SiteNav.tsx
@@ -1,6 +1,10 @@
+import { Link } from "react-router-dom"
 
 export default function SiteNav () {
     return (
-        <div>Nav</div>
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+        </div>
     )
 }
